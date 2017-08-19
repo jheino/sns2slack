ae57f6485d694ea9f0bcdebe9bd11ac6807bb7a4
Jukka Heino <jukka.heino@gmail.com>
2017-08-19 22:58:33 +0300

    Use variables for specifying S3 bucket and webhook URL

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+S3_BUCKET=
+WEBHOOK_URL=
+
 aws cloudformation package \
 	--template-file sns2slack.yaml \
-	--s3-bucket jheino-lambda \
+	--s3-bucket ${S3_BUCKET} \
 	--output-template-file sns2slack-output.yaml
 
 aws cloudformation deploy \
 	--template-file sns2slack-output.yaml \
 	--stack-name SNS2Slack \
 	--capabilities CAPABILITY_IAM \
-	--parameter-overrides WebhookUrl=https://hooks.slack.com/services/...
+	--parameter-overrides WebhookUrl=${WEBHOOK_URL}
